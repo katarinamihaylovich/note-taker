ac1dfddd37e3878f248fec748681539e784dd3fc
Lulu Jovovich <lulujovovich@Lulus-MBP.lan>
2022-09-21 12:33:21 -0700

    initial changes

---
+++ Develop/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+})
+
+
+
+
+
+
+app.listen(3001, () => {
+    console.log('app now running on http://localhost:3001/');
+});
