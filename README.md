52ae49a44107e306f98b492710bc39141ff48dc8
Lulu Jovovich <lulujovovich@Lulus-MBP.lan>
2022-09-21 21:34:37 -0700

    main readme info and some tweaks

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # note-taker
+
+## Description
+
+The purpose of this project was to present an application consisting of a home page and a functional link to a notes page so that a user would be able to write and save notes. I wanted to create this project to demonstrate, via express, how front- and back-end development work together via middleware. I was given starter code and created an index.js file that would function as my server. The only other file created was an fsUtils.js file in the "helpers" folder. My goal was to create a way for users to add, read, and delete notes. I did this by establishing various routes between files by incorporating the assistance of uuid and express.js. I learned more about how files interact with one another through the use of express, how uuid creates unique and necessary ids, and how ports work.
+
+## Technology Used:
+1. Javascript
+2. HTML
+3. CSS
+4. Node
+5. Express
+6. Bootstrap
+7. Fontawesome
+8. uuid
+
+## Link to Glitch deployment:
+
+
+## Screenshot:
+
+![](/images/Screen%20Shot%202022-09-21%20at%209.32.10%20PM.png)
--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+  } = require('./helpers/fsUtils');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -9,18 +16,51 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+// Route to home page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+//Route to notes page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 })
 
+//Route to read and parse data from the database/notes page
+app.get('/api/notes', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
 
+//Route to post new notes
+app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
 
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
 
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added!`);
+    } else {
+        res.error('Error!');
+    }
+});
 
+//Route to delete notes
+app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            const result = json.filter((note) => note.id !== noteId);
+
+            writeToFile('./db/db.json', result);
+            res.json(`Note ${noteId} has been deleted`);
+        })
+});
 
 app.listen(PORT, () => {
     console.log('app now running on http://localhost:3001/');
